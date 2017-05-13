4c0cd32e58d0d9d8ec2e0cc1d5cd90e2d7fbefb6
blogscot <iain.diamond@gmail.com>
2017-05-13 21:26:55 +0100

    Add Nucleotide Count

---
+++ nucleotide-count/src/lib.rs
@@ -0,0 +1,25 @@
+use std::collections::HashMap;
+
+pub fn count(nucleo: char, sequence: &str) -> Result<usize, &str> {
+    if !is_valid(nucleo) || !is_valid_sequence(sequence) {
+        return Err("Invalid Symbol found!");
+    }
+    Ok(sequence.chars().filter(|s| *s == nucleo).count())
+}
+
+pub fn nucleotide_counts(sequence: &str) -> Result<HashMap<char, usize>, &str> {
+    let mut letters = HashMap::new();
+    letters.insert('A', count('A', sequence)?);
+    letters.insert('C', count('C', sequence)?);
+    letters.insert('G', count('G', sequence)?);
+    letters.insert('T', count('T', sequence)?);
+    Ok(letters)
+}
+
+fn is_valid_sequence(sequence: &str) -> bool {
+    sequence.chars().all(|s| is_valid(s))
+}
+
+fn is_valid(nucleo: char) -> bool {
+    "ACGT".contains(nucleo)
+}
