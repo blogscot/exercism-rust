014b1d12e20aab61b59774033a97c19f70524057
blogscot <iain.diamond@gmail.com>
2017-05-15 12:29:23 +0100

    Add Word Count

---
+++ word-count/src/lib.rs
@@ -0,0 +1,19 @@
+use std::collections::HashMap;
+
+pub fn word_count(text: &str) -> HashMap<String, u32> {
+    let cleaned = clean(text.to_lowercase());
+    let mut words = HashMap::new();
+
+    for word in cleaned.split_terminator(' ') {
+        let counter = words.entry(word.into()).or_insert(0);
+        *counter += 1;
+    }
+    words
+}
+
+fn clean(text: String) -> String {
+    let text = text.chars()
+        .filter(|&ch| ch.is_alphanumeric() || ch == ' ')
+        .collect::<String>();
+    text.replace("  ", " ")
+}
