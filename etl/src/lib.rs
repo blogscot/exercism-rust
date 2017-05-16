1bb580c06825062a7076dafa80ffe79798851cb4
blogscot <iain.diamond@gmail.com>
2017-05-16 18:52:42 +0100

    Add Etl

---
+++ etl/src/lib.rs
@@ -0,0 +1,12 @@
+use std::collections::BTreeMap;
+
+pub fn transform(input: &BTreeMap<i32, Vec<String>>) -> BTreeMap<String, i32> {
+    input
+        .iter()
+        .flat_map(|(k, vec)| {
+                      vec.into_iter()
+                          .map(|v| (v.to_lowercase(), *k))
+                          .collect::<Vec<_>>()
+                  })
+        .collect()
+}
