553f0c4efb9bb4fd25f5d643715c9256fe0b4f46
blogscot <iain.diamond@gmail.com>
2017-05-17 21:28:27 +0100

    Refactor Acronym

--- acronym/src/lib.rs
+++ acronym/src/lib.rs
@@ -2,14 +2,10 @@ extern crate regex;
 use regex::Regex;
 
 pub fn abbreviate(text: &str) -> String {
-    let re = Regex::new(r"[A-Z][a-z]|\b[A-Z]|\b\w").unwrap();
+    let re = Regex::new(r"\b\w|[A-Z][a-z]").unwrap();
     let mut result: Vec<String> = Vec::new();
     for caps in re.captures_iter(text) {
-        save(&mut result, &caps[0][..1]);
+        result.push(caps[0][..1].into());
     };
     result.join("").to_uppercase()
-}
-
-fn save(store: &mut Vec<String>, value: &str) {
-    store.push(value.into());
 }
