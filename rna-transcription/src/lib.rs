7c9f64b1b650805979a8c9418aedd26315235e81
blogscot <iain.diamond@gmail.com>
2017-05-18 21:45:05 +0100

    Add RNA transcription

---
+++ rna-transcription/src/lib.rs
@@ -0,0 +1,40 @@
+#[derive(Debug, PartialEq)]
+pub struct RibonucleicAcid {
+    rna: String,
+}
+
+impl RibonucleicAcid {
+    pub fn new(rna: &str) -> Self {
+        RibonucleicAcid { rna: rna.into() }
+    }
+}
+
+#[derive(Debug, PartialEq)]
+pub struct DeoxyribonucleicAcid {
+    dna: String,
+}
+
+impl DeoxyribonucleicAcid {
+    pub fn new(dna: &str) -> Self {
+        DeoxyribonucleicAcid { dna: dna.into() }
+    }
+
+    pub fn to_rna(&self) -> RibonucleicAcid {
+        let rna = self.dna
+            .chars()
+            .map(|ch| convert(ch))
+            .collect::<String>();
+
+        RibonucleicAcid { rna: rna }
+    }
+}
+
+fn convert(chr: char) -> char {
+    match chr {
+        'C' => 'G',
+        'G' => 'C',
+        'A' => 'U',
+        'T' => 'A',
+        _ => chr,
+    }
+}
