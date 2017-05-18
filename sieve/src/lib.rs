c4ac906c4f7b1d6c4e335bfbea0680725a89b16d
blogscot <iain.diamond@gmail.com>
2017-05-18 20:36:56 +0100

    Add Sieve

---
+++ sieve/src/lib.rs
@@ -0,0 +1,19 @@
+pub fn primes_up_to(limit: usize) -> Vec<usize> {
+    let mut primes: Vec<usize> = (1..limit + 1).collect();
+    primes[0] = 0;
+
+    for value in series(limit) {
+        for i in range_step(value + 1, limit, value) {
+            primes[i - 1] = 0;
+        }
+    }
+    primes.into_iter().filter(|x| *x > 0).collect()
+}
+
+fn series(limit: usize) -> Vec<usize> {
+    (2..).take_while(|x| x * x <= limit).collect()
+}
+
+fn range_step(start: usize, end: usize, step: usize) -> Vec<usize> {
+    (start..end + 1).filter(|x| x % step == 0).collect()
+}
