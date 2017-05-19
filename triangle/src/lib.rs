18258ba8007ffc5bd37a6c6439a1e9fa3c71221e
blogscot <iain.diamond@gmail.com>
2017-05-19 16:23:59 +0100

    Add Triangle

---
+++ triangle/src/lib.rs
@@ -0,0 +1,35 @@
+extern crate num;
+use num::Num;
+
+#[derive(PartialEq)]
+pub enum Triangle {
+    Equilateral,
+    Isosceles,
+    Scalene,
+}
+
+impl Triangle {
+    pub fn build(mut sides: [u32; 3]) -> Result<Self, String> {
+        sides.sort();
+        let sides = (sides[0], sides[1], sides[2]);
+        match sides {
+            (0, 0, 0) => Err("Invalid triangle".into()),
+            (a, b, c) if a + b < c => Err("Invalid triangle".into()),
+            (a, b, c) if a == b && b == c => Ok(Triangle::Equilateral),
+            (a, b, c) if a == b || b == c || a == c => Ok(Triangle::Isosceles),
+            (_, _, _) => Ok(Triangle::Scalene),
+        }
+    }
+
+    pub fn is_equilateral(&self) -> bool {
+        *self == Triangle::Equilateral
+    }
+
+    pub fn is_isosceles(&self) -> bool {
+        *self == Triangle::Isosceles
+    }
+
+    pub fn is_scalene(&self) -> bool {
+        *self == Triangle::Scalene
+    }
+}
