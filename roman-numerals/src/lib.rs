bc56741f872ddb19b968c005e2e49f69aa1d5d52
blogscot <iain.diamond@gmail.com>
2017-05-20 16:25:56 +0100

    Add Roman Numerals

---
+++ roman-numerals/src/lib.rs
@@ -0,0 +1,33 @@
+pub struct Roman {
+    symbol: &'static str,
+    value: u32,
+}
+
+const NUMERALS: [Roman; 13] = [
+    Roman { symbol: "I",  value: 1 },
+    Roman { symbol: "IV", value: 4 },
+    Roman { symbol: "V",  value: 5 },
+    Roman { symbol: "IX", value: 9 },
+    Roman { symbol: "X",  value: 10 },
+    Roman { symbol: "XL", value: 40 },
+    Roman { symbol: "L",  value: 50 },
+    Roman { symbol: "XC", value: 90 },
+    Roman { symbol: "C",  value: 100 },
+    Roman { symbol: "CD", value: 400 },
+    Roman { symbol: "D",  value: 500 },
+    Roman { symbol: "CM", value: 900 },
+    Roman { symbol: "M",  value: 1000 },
+];
+
+impl Roman {
+    pub fn from(mut number: u32) -> String {
+        let mut roman: String = String::new();
+        for numeral in NUMERALS.iter().rev() {
+            while number >= numeral.value {
+                roman = roman + numeral.symbol;
+                number -= numeral.value;
+            }
+        }
+        roman
+    }
+}
