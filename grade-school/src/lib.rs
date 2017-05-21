195268ae49133db2843fb68822974180e88bdf01
blogscot <iain.diamond@gmail.com>
2017-05-21 20:01:47 +0100

    Add Grade School

---
+++ grade-school/src/lib.rs
@@ -0,0 +1,27 @@
+use std::collections::HashMap;
+
+pub struct School {
+    students: HashMap<u32, Vec<String>>,
+}
+
+impl School {
+    pub fn new() -> School {
+        School { students: HashMap::new() }
+    }
+
+    pub fn add(&mut self, grade: u32, student: &str) {
+        let grade = self.students.entry(grade).or_insert(Vec::new());
+        grade.push(student.into());
+        grade.sort();
+    }
+
+    pub fn grades(&self) -> Vec<u32> {
+        let mut grades: Vec<u32> = self.students.keys().map(|&key| key).collect();
+        grades.sort();
+        grades
+    }
+
+    pub fn grade(&self, grade: u32) -> Option<Vec<String>> {
+        self.students.get(&grade).cloned()
+    }
+}
