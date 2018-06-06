56e0af308e715ffd83fbdf84cb796e987d91c51a
Iain Diamond <iain.diamond@gmail.com>
2018-06-06 20:12:21 +0100

    Refactor Luhn

--- luhn-from/src/lib.rs
+++ luhn-from/src/lib.rs
@@ -1,46 +1,45 @@
 #[derive(Debug)]
 pub struct Luhn {
-    text: String,
+  text: String,
 }
 
 impl<T: ToString> From<T> for Luhn {
-    fn from(input: T) -> Self {
-        Luhn {
-            text: input.to_string(),
-        }
+  fn from(input: T) -> Self {
+    Luhn {
+      text: input.to_string(),
     }
+  }
 }
 
 impl Luhn {
-    pub fn is_valid(&self) -> bool {
-        let cleaned = self.text.replace(" ", "");
-        let is_valid_sequence = |seq: &str| seq.chars().all(char::is_numeric);
+  pub fn is_valid(&self) -> bool {
+    let cleaned = self.text.replace(" ", "");
+    let is_valid_sequence = |seq: &str| cleaned.len() > 1 && seq.chars().all(char::is_numeric);
 
-        if cleaned.len() <= 1 || !is_valid_sequence(&cleaned) {
-            false
-        } else {
-            Self::calculate_sum(&cleaned) % 10 == 0
+    if is_valid_sequence(&cleaned) {
+      return Self::calculate_sum(&cleaned) % 10 == 0;
+    }
+    false
+  }
+
+  fn calculate_sum(sequence: &str) -> u32 {
+    let is_odd = |x| x % 2 != 0;
+
+    sequence
+      .chars()
+      .rev()
+      .enumerate()
+      .map(|(index, chr)| {
+        let mut value = chr.to_digit(10).unwrap();
+
+        if is_odd(index) {
+          value *= 2;
+          if value > 9 {
+            value -= 9;
+          }
         }
-    }
-
-    fn calculate_sum(sequence: &str) -> u32 {
-        let is_odd = |x| x % 2 != 0;
-
-        sequence
-            .chars()
-            .rev()
-            .enumerate()
-            .map(|(k, v)| {
-                let mut value = v.to_digit(10).unwrap();
-
-                if is_odd(k) {
-                    value *= 2;
-                    if value > 9 {
-                        value -= 9;
-                    }
-                }
-                value
-            })
-            .sum()
-    }
+        value
+      })
+      .sum()
+  }
 }
