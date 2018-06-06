74d5fcc4871e1619c72e597ab37bfe897f812a51
Iain Diamond <iain.diamond@gmail.com>
2018-06-06 20:16:55 +0100

    Refactor Luhn

--- luhn/src/lib.rs
+++ luhn/src/lib.rs
@@ -1,34 +1,34 @@
 pub fn is_valid(sequence: &str) -> bool {
-    let cleaned = sequence.replace(" ", "");
-    if cleaned.len() <= 1 || !is_valid_sequence(&cleaned) {
-        return false
-    }
-    calculate_sum(&cleaned) % 10 == 0
+  let cleaned = sequence.replace(" ", "");
+  if cleaned.len() > 1 && is_valid_sequence(&cleaned) {
+    return calculate_sum(&cleaned) % 10 == 0;
+  }
+  false
 }
 
 fn calculate_sum(sequence: &str) -> u32 {
-    sequence
-        .chars()
-        .rev()
-        .enumerate()
-        .map(|(k, v)| {
-            let mut value = v.to_digit(10).unwrap();
+  sequence
+    .chars()
+    .rev()
+    .enumerate()
+    .map(|(index, chr)| {
+      let mut value = chr.to_digit(10).unwrap();
 
-            if is_odd(k) {
-                value = value * 2;
-                if value > 9 {
-                    value -= 9;
-                }
-            }
-            value
-        })
-        .sum::<u32>()
+      if is_odd(index) {
+        value *= 2;
+        if value > 9 {
+          value -= 9;
+        }
+      }
+      value
+    })
+    .sum::<u32>()
 }
 
 fn is_valid_sequence(sequence: &str) -> bool {
-    sequence.chars().all(char::is_numeric)
+  sequence.chars().all(char::is_numeric)
 }
 
 fn is_odd(x: usize) -> bool {
-    x % 2 != 0
+  x % 2 != 0
 }
