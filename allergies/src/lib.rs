f69da0fe72088090bbae64dcf8a93ea09ae59af3
Iain Diamond <iain.diamond@gmail.com>
2018-06-10 16:19:19 +0100

    Perfer Static data

--- allergies/src/lib.rs
+++ allergies/src/lib.rs
@@ -12,30 +12,28 @@ pub enum Allergen {
 
 use Allergen::*;
 
+static ALLERGENS: [Allergen; 8] = [
+  Eggs,
+  Peanuts,
+  Shellfish,
+  Strawberries,
+  Tomatoes,
+  Chocolate,
+  Pollen,
+  Cats,
+];
+
 pub struct Allergies {
   allergies: Vec<Allergen>,
 }
 
 impl Allergies {
-  fn get_allergens() -> Vec<Allergen> {
-    vec![
-      Eggs,
-      Peanuts,
-      Shellfish,
-      Strawberries,
-      Tomatoes,
-      Chocolate,
-      Pollen,
-      Cats,
-    ]
-  }
   pub fn new(input: u32) -> Self {
-    let allergens = Self::get_allergens();
     let mut allergies = vec![];
-    for allergen in allergens {
-      let value = 1u32.rotate_left(allergen as u32);
+    for allergen in &ALLERGENS {
+      let value = 1 << *allergen as u32;
       if value & input > 0 {
-        allergies.push(allergen);
+        allergies.push(*allergen);
       }
     }
     Allergies { allergies }
