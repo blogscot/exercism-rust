a4b26ef77c1addad87002c7236905e0938a091a4
Iain Diamond <iain.diamond@gmail.com>
2018-06-10 15:22:38 +0100

    Add Macros solution

---
+++ macros/src/lib.rs
@@ -0,0 +1,16 @@
+#[macro_export]
+macro_rules! hashmap {
+  () => {
+    HashMap::new()
+  };
+
+  ( $( $key:expr => $value:expr $(,)* ), *) => {
+    {
+      let mut hm = HashMap::new();
+      $(
+          hm.insert($key, $value);
+      )*
+      hm
+    }
+  };
+}
