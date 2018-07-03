3a4e2e1f53af5459ef23be4b8aead217c54301c0
Iain Diamond <iain.diamond@gmail.com>
2018-07-03 18:59:24 +0100

    Refactor solution

--- diamond/src/lib.rs
+++ diamond/src/lib.rs
@@ -1,10 +1,11 @@
-fn get_template_string(letter: char) -> (String, Vec<String>) {
-    let letter_as_string = letter.to_string();
-    let alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-    let alphabet: Vec<String> = alphabet.chars().map(|letter| letter.to_string()).collect();
+fn get_template_string(chr: char) -> (String, Vec<String>) {
+    let alphabet: Vec<String> = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+        .chars()
+        .map(|letter| letter.to_string())
+        .collect();
     let letter_position = alphabet
         .iter()
-        .position(|ref letter| letter == &&letter_as_string)
+        .position(|letter| letter == &chr.to_string())
         .unwrap();
     let forward_pattern = &alphabet[0..=letter_position];
     let reverse_pattern: Vec<String> = forward_pattern.iter().cloned().skip(1).rev().collect();
@@ -14,7 +15,7 @@ fn get_template_string(letter: char) -> (String, Vec<String>) {
 
 pub fn get_diamond(letter: char) -> Vec<String> {
     let (template, forward_pattern) = get_template_string(letter);
-    let mut output = vec![];
+    let mut top = vec![];
     for letter in forward_pattern {
         let filtered: String = template
             .chars()
@@ -27,10 +28,10 @@ pub fn get_diamond(letter: char) -> Vec<String> {
                 }
             })
             .collect();
-        output.push(filtered);
+        top.push(filtered);
     }
-    let mut diamond = output.clone();
-    let diamond_bottom: Vec<String> = output.iter().cloned().rev().skip(1).collect();
-    diamond.extend(diamond_bottom);
+    let mut diamond = top.clone();
+    let bottom: Vec<String> = top.iter().cloned().rev().skip(1).collect();
+    diamond.extend(bottom);
     diamond
 }
