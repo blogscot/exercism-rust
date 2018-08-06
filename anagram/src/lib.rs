9632b4a9fff946848c4feb34f70d4797d8496bde
Iain Diamond <iain.diamond@gmail.com>
2018-08-06 19:56:58 +0100

    Add Anagram solution

---
+++ anagram/src/lib.rs
@@ -0,0 +1,24 @@
+use std::collections::HashSet;
+
+pub fn anagrams_for<'a>(word: &str, possible_anagrams: &'a [&str]) -> HashSet<&'a str> {
+  let anagrams = possible_anagrams.to_vec();
+  anagrams
+    .iter()
+    .cloned()
+    .filter(|&anagram| anagram.to_lowercase() != word.to_lowercase() && compare(anagram, word))
+    .collect()
+}
+
+fn compare(left: &str, right: &str) -> bool {
+  let mut right = right
+    .chars()
+    .map(|chr| chr.to_lowercase().to_string())
+    .collect::<Vec<_>>();
+  let mut left = left
+    .chars()
+    .map(|chr| chr.to_lowercase().to_string())
+    .collect::<Vec<_>>();
+  left.sort();
+  right.sort();
+  left == right
+}
