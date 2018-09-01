cc7f36fcf47c58647be21588ec9ed0068e022f62
Iain Diamond <iain.diamond@gmail.com>
2018-09-01 16:47:51 +0100

    Refactor solution

--- parallel-letter-frequency/src/lib.rs
+++ parallel-letter-frequency/src/lib.rs
@@ -2,20 +2,15 @@ use std::sync::mpsc;
 use std::sync::mpsc::Sender;
 use std::{collections::HashMap, thread};
 
-pub type LetterCount = HashMap<char, u32>;
+type LetterCount = HashMap<char, u32>;
 
 pub fn frequency<'a>(text: &'a [&'static str], num_workers: usize) -> LetterCount {
-  if text.len() < 128 {
+  if text.len() < 32 {
     return frequency_helper(text);
   }
+
   let (tx, rx) = mpsc::channel();
-
-  let chunks = build_chunks(text, num_workers)
-    .into_iter()
-    .filter(|chunk| !chunk.is_empty())
-    .collect::<Vec<_>>();
-
-  for chunk in chunks {
+  for chunk in build_chunks(text, num_workers) {
     let tx1 = Sender::clone(&tx);
     thread::spawn(move || tx1.send(frequency_helper(&chunk)).unwrap());
   }
@@ -46,7 +41,7 @@ fn add(mut list1: LetterCount, list2: LetterCount) -> LetterCount {
   list1
 }
 
-pub fn frequency_helper(texts: &[&str]) -> LetterCount {
+fn frequency_helper(texts: &[&str]) -> LetterCount {
   let mut map = HashMap::new();
 
   for line in texts {
